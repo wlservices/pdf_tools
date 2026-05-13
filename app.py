71f245ea2f8d6d0f9087fdc3f17844be90b4a340
LuanCAlves <luan_dev@outlook.com.br>
2026-05-13 11:57:32 -0300

    fix: Message sending feedback modal working correctly

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.config['MAIL_PORT'] = os.getenv("MAIL_PORT")
 app.config['MAIL_USE_SSL']=os.getenv("MAIL_USE_SSL")
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+app.config['MAIL_PASSWORD'] = 'U!(UqhJ09'
 
 mail = Mail(app)
 
